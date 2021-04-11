82243f02dee298154fab46149e1f5289e1d3b012
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-11 17:31:50 -0400

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=3ad13624`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
@@ -28,14 +30,11 @@ const App = () => {
 				<MovieListHeading heading='Movies' />
 				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
 			</div>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Favourites' />
-			</div>
-			<div className="row">
-				<MovieList />
+			<div className='row'>
+				<MovieList movies={movies} />
 			</div>
 		</div>
-	)
-}
+	);
+};
 
 export default App;
