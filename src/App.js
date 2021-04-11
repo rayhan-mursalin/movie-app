d7e95dccc437cd9887b076219fa34181e4c0f084
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-11 17:23:30 -0400

    Add search value

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieListHeading from './components/MovieListHeading';
-
-
+import SearchBox from './components/SearchBox';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
@@ -19,8 +18,16 @@ const App = () => {
 		}
 	};
 
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
 	return (
 		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
 			<div className='row d-flex align-items-center mt-4 mb-4'>
 				<MovieListHeading heading='Favourites' />
 			</div>
