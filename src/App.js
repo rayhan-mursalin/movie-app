73d16a2b4837e6fbb7f3c3d36f2ba257d94d4da7
Rayhan Mursalin <rmursalin@webmd.net>
2021-04-11 17:09:14 -0400

    Get movies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
 
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=3ad13624`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
 	return (
-		<MovieList></MovieList>
+		<div className='container-fluid movie-app'>
+			<div className="row">
+				<MovieList />
+			</div>
+		</div>
 	)
 }
 
